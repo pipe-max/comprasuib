65731ddc4aec99d88ae8fe5b5ec0c59640544129
Pipe <pipe@theodoro.edu.co>
2026-05-14 14:11:17 -0500

    cambios

--- functions/index.js
+++ functions/index.js
@@ -119,7 +119,6 @@ exports.sendOrderEmail = onRequest(
                 from: '"Contabilidad UIB" <pipe@theodoro.edu.co>',
                 to,
                 cc: cc || '',
-                bcc: 'pipe@theodoro.edu.co',
                 subject,
                 text: body,
             };
