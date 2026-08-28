bbd003bb469d9a2e563a86be82a9148aac63aad2
Pipe <pipe@theodoro.edu.co>
2026-08-28 08:02:05 -0500

    Agregar apis.google.com a connect-src (ping de verificación del login con Google)

--- _worker.js
+++ _worker.js
@@ -8,7 +8,7 @@ const CSP = [
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "font-src 'self' https://fonts.gstatic.com",
     "img-src 'self' data: blob: https:",
-    "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://*.cloudfunctions.net https://firebasestorage.googleapis.com https://unpkg.com https://cdn.jsdelivr.net https://www.gstatic.com",
+    "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://*.cloudfunctions.net https://firebasestorage.googleapis.com https://unpkg.com https://cdn.jsdelivr.net https://www.gstatic.com https://apis.google.com",
     "frame-src 'self' https://compras-cth.firebaseapp.com https://accounts.google.com https://firebasestorage.googleapis.com",
     "object-src 'none'",
     "base-uri 'self'",
